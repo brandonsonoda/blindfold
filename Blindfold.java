c06c23b3147c9f0b480a3379e6de23c7246b73b5
Brandon Sonoda <sonodabe@gmail.com>
2023-07-09 01:05:55 -0400

    Break Blindfold into its own class

--- Blindfold.java
+++ Blindfold.java
@@ -1,52 +1,5 @@
-import java.util.ArrayList;
-import java.util.stream.Stream;
-import java.util.Iterator;
-import java.util.Scanner;
-
-// Blind fold notation
-// Scramble, Blindfold notation
 public class Blindfold {
-  private static final String BLIND_PREFIX = "blind: ";
-  public static void main(String[] args) {
-    Cube c = new Cube();
-    Scanner scanner = new Scanner(System.in);
-    while (sneakyPrint() && scanner.hasNextLine()) {
-      String line = scanner.nextLine();
-      if (line.equalsIgnoreCase("print") || line.equalsIgnoreCase("ls")) {
-        System.out.println(c);
-        continue;
-      }
-
-      if (line.equalsIgnoreCase("clear")) {
-        c.setCubeToSolved();
-        continue;
-      }
-
-      if (line.equalsIgnoreCase("is_solved")) {
-        System.out.println(c.isCubeSolved());
-        continue;
-      }
-
-      if (line.toLowerCase().startsWith(BLIND_PREFIX.toLowerCase())) {
-        blindfoldSolve(c, line.substring(BLIND_PREFIX.length()));
-        continue;
-      }
-
-      try {
-        c.apply(Algorithms.compile(line));
-      } catch (IllegalArgumentException e) {
-        System.out.println("invalid notation");
-      }
-    }
-    System.out.println();
-  }
-
-  private static boolean sneakyPrint() {
-    System.out.print(">: ");
-    return true;
-  }
-
-  public static void blindfoldSolve(Cube c, String memo) {
+  public static void apply(Cube c, String memo) {
     String[] split = memo.trim().split("\\s+");
     if (split.length > 3) {
       throw new IllegalArgumentException("Too many arguments to blind!");
@@ -92,6 +45,5 @@ public class Blindfold {
       c.apply(Algorithms.CORNER_SWAP);
       c.undo(SetupMoves.CORNER_SETUPS.get(corner));
     }
-
   }
 }
---
+++ Main.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.stream.Stream;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+  private static final String BLIND_PREFIX = "blind: ";
+
+  public static void main(String[] args) {
+    Cube c = new Cube();
+    Scanner scanner = new Scanner(System.in);
+    while (sneakyPrint() && scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+      if (line.equalsIgnoreCase("print") || line.equalsIgnoreCase("ls")) {
+        System.out.println(c);
+        continue;
+      }
+
+      if (line.equalsIgnoreCase("clear")) {
+        c.setCubeToSolved();
+        continue;
+      }
+
+      if (line.equalsIgnoreCase("is_solved")) {
+        System.out.println(c.isCubeSolved());
+        continue;
+      }
+
+      if (line.toLowerCase().startsWith(BLIND_PREFIX.toLowerCase())) {
+        Blindfold.apply(c, line.substring(BLIND_PREFIX.length()));
+        continue;
+      }
+
+      try {
+        c.apply(Algorithms.compile(line));
+      } catch (IllegalArgumentException e) {
+        System.out.println("invalid notation");
+      }
+    }
+    System.out.println();
+  }
+
+  private static boolean sneakyPrint() {
+    System.out.print(">: ");
+    return true;
+  }
+}
