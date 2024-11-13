56422fe409cc525a8bc425359dd9b6916d9dd2bb
Brandon Sonoda <sonodabe@gmail.com>
2024-11-12 23:06:20 -0500

    Add comment help to the main functions of the program

--- Main.java
+++ Main.java
@@ -8,36 +8,43 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     while (sneakyPrint() && scanner.hasNextLine()) {
       String line = scanner.nextLine();
+      // Print the current state of the cube
       if (line.equalsIgnoreCase("print") || line.equalsIgnoreCase("ls")) {
         System.out.println(c);
         continue;
       }
 
+      // Return the current cube to the solved position
       if (line.equalsIgnoreCase("clear")) {
         c.setCubeToSolved();
         continue;
       }
 
+      // Switch to rendering the cube as colored squares
       if (line.equalsIgnoreCase("color")) {
         Color.setPrintMode(Color.PrintMode.COLOR);
         continue;
       }
 
+      // Switch to rendering the cube as numbered squares
       if (line.equalsIgnoreCase("text")) {
         Color.setPrintMode(Color.PrintMode.TEXT);
         continue;
       }
 
+      // Returns true if the cube is solved, false otherwise
       if (line.equalsIgnoreCase("is_solved")) {
         System.out.println(c.isCubeSolved());
         continue;
       }
 
+      // Applies blind solving notation to the cube
       if (line.toLowerCase().startsWith(BLIND_PREFIX.toLowerCase())) {
         Blindfold.apply(c, line.substring(BLIND_PREFIX.length()));
         continue;
       }
 
+      // Default apply notation to the cube
       try {
         c.apply(Algorithms.compile(line));
       } catch (IllegalArgumentException e) {
